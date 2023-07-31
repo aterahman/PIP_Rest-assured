664ece99ef6cd76fdcc6b083226e9ef8936131a9
aterahman <aterahman@deloitte.com>
2023-07-31 20:25:43 +0530

    added test cases to validate json schema and check for the number of products

--- src/test/java/TestCases/CartApiDetails/GetPayloadTest.java
+++ src/test/java/TestCases/CartApiDetails/GetPayloadTest.java
@@ -2,35 +2,54 @@ package TestCases.CartApiDetails;
 
 import Utils.ExtentTestGenerator;
 import Utils.Response_Payload_Array_Generator;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.json.JSONArray;
-import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
+@Listeners
 //class with all the tests related to the get method wrt User Api Details
 public class GetPayloadTest extends ExtentTestGenerator
 {
-    public JSONArray Payload_array;
-
-    //test to check if the payload response status code is 200
-   @BeforeClass
-    public void get_response_status_code()
+    //test to get response payload and validate json schema
+    @Test
+    public void validate_json_scehma()
     {
         given().
                 baseUri("https://fakestoreapi.com").
                 log().all().
-
                 when().
                 get("/carts").
                 then().
-                log().all().
-                statusCode(200);
+                statusCode(200).
+                and().
+                assertThat().
+                body(JsonSchemaValidator.matchesJsonSchemaInClasspath("carts_schema.json"));
 
-        Response_Payload_Array_Generator generator = new Response_Payload_Array_Generator();
-        JSONArray arr = generator.get_response_payload("https://fakestoreapi.com","/carts");
-
-        this.Payload_array = arr;
     }
-    
+
+    //test to check the number of products in payload and validate if they are atleast 1
+    @Test
+    public void validate_number_of_products()
+    {
+        Response_Payload_Array_Generator generator = new Response_Payload_Array_Generator();
+        JSONArray array = generator.get_response_payload("https://fakestoreapi.com","/carts");
+
+        for(int i = 0; i< array.length(); i++)
+        {
+            JSONArray products = array.getJSONObject(i).getJSONArray("products");
+
+            for(int j=0;j<products.length();j++)
+            {
+                int productcount = Integer.parseInt(products.getJSONObject(j).get("quantity").toString());
+
+                Assert.assertTrue(productcount>=1);
+            }
+
+
+        }
+    }
 }
